13091523ded11543f396440b8bf018d0ca3d12ac
abdulrahmaniliyasu <abdulrahmaniliyasu86@gmail.com>
2020-11-19 23:26:27 +0100

    Website update.

--- export.sh
+++ export.sh
@@ -2,5 +2,5 @@
 
 git add --all
 git commit -m "Website update."
-git remote add origin https://github.com/retrx0/retrx0.github.io.git
+# git remote add origin https://github.com/retrx0/retrx0.github.io.git
 git push -u origin main
